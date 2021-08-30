efe1637d7638e0fd70fd74f3141105b1c91fb4ae
Mitch Hankins <mitchhankins@icloud.com>
2021-08-30 18:12:23 -0400

    cleanup

--- src/views/home/HomeView/index.js
+++ src/views/home/HomeView/index.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
 import Hero from './Hero';
 import Features from './Features';
-import Testimonials from './Testimonials';
+// import Testimonials from './Testimonials';
 import CTA from './CTA';
 import FAQS from './FAQS';
 
