2921a4430b7684842b4a4a62902dd20bcd1468c0
Mitch Hankins <mitchhankins@icloud.com>
2021-08-30 18:01:15 -0400

    fetch dividends in auth

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,7 +5,7 @@ import React, {
   useCallback
 } from 'react';
 import { Auth, Cache, API, graphqlOperation } from 'aws-amplify';
-import { listStatistics } from 'src/graphql/queries';
+import { listStatistics, listDividends } from 'src/graphql/queries';
 import useIsMountedRef from 'src/hooks/useIsMountedRef';
 
 const initialAuthState = {
@@ -15,6 +15,10 @@ const initialAuthState = {
     costBasis: 0,
     marketValue: 0,
     totalDividends: 0
+  },
+  listDividends: {
+    all: [],
+    upcoming: []
   }
 };
 
@@ -32,6 +36,9 @@ export const AuthProvider = ({ children }) => {
   const [state, setState] = useState(initialAuthState);
   const { isAuthenticated } = state;
 
+  /*
+    Cognito
+  */
   const login = async (email, password) => {
     await Auth.signIn({ username: email, password });
     setState({ ...initialAuthState, isAuthenticated: true });
@@ -61,6 +68,9 @@ export const AuthProvider = ({ children }) => {
     });
   };
 
+  /*
+    Data
+  */
   const getStatistics = useCallback(async () => {
     const processStatistics = list => {
       console.log('processing statistics');
@@ -98,6 +108,45 @@ export const AuthProvider = ({ children }) => {
     }
   }, [isMountedRef]);
 
+  const getDividends = useCallback(async () => {
+    const processDividends = list => {
+      console.log('processing dividends');
+
+      const upcoming = [];
+      const yesterday = new Date().setDate(new Date().getDate() - 1);
+      list.forEach(item => {
+        if (new Date(item.paymentDate) > yesterday) {
+          upcoming.push(item);
+        }
+      });
+
+      setState(s => ({ ...s, listDividends: { all: list, upcoming  } }));
+    }
+
+    if (isMountedRef.current) {
+      const cachedListDividends = Cache.getItem('listDividends');
+
+      if (cachedListDividends) {
+        console.log('listDividends already cached');
+        processDividends(cachedListDividends);
+      } else {
+        console.log('caching listDividends');
+
+        const { data } = await API.graphql(graphqlOperation(listDividends));
+        const parsed = JSON.parse(data.listDividends);
+        Cache.setItem(
+          'listDividends',
+          parsed,
+          { expires: new Date().setHours(new Date().getHours() + 1) }
+        );
+        processDividends(parsed);
+      }
+    }
+  }, [isMountedRef]);
+
+  /*
+    Effects
+  */
   useEffect(() => {
     console.log('init');
     const intialize = async () => {
@@ -119,8 +168,9 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     console.log('isAuthenticated effect auth: ', isAuthenticated);
     if (isAuthenticated) {
-      console.log('auth yes, getStatistics');
+      console.log('auth yes, getStatistics - getDividends');
       getStatistics();
+      getDividends();
     }
   }, [isAuthenticated, getStatistics]);
 
