759e57ba1432cdd2e378e8c0b5b8ea33ed8c1533
Mitch Hankins <mitchhankins@icloud.com>
2021-10-02 17:49:21 -0400

    calendar total

--- src/views/dividends/CalendarView/Header.js
+++ src/views/dividends/CalendarView/Header.js
@@ -6,11 +6,12 @@ import {
   Grid,
   Link,
   Typography,
-  makeStyles
+  makeStyles,
 } from '@material-ui/core';
 import { format } from 'date-fns';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import Toolbar from './Toolbar';
+import formatter from 'src/utils/numberFormatter';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,6 +28,7 @@ const Header = ({
   onDateToday,
   onViewChange,
   view,
+  totalMonthlyDividends,
   ...rest
 }) => {
   const classes = useStyles();
@@ -63,6 +65,20 @@ const Header = ({
           color="textPrimary"
         >
           {format(date, 'MMMM yyyy')}
+        </Typography>
+      </Grid>
+      <Grid item >
+        <Typography
+          variant="subtitle2"
+          color="inherit"
+        >
+          {`${format(date, 'MMMM')} Total:`}
+        </Typography>
+        <Typography
+          variant="h3"
+          color="textPrimary"
+        >
+          {formatter.format(totalMonthlyDividends)}
         </Typography>
       </Grid>
       <Grid item>
