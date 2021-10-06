caca0c1dba64e76c7f0dcdbcae44b3232e5efa2f
Mitch Hankins <mitchhankins@icloud.com>
2021-10-06 14:00:09 -0400

    ses

--- amplify/backend/function/stripeHookFunction/src/app.js
+++ amplify/backend/function/stripeHookFunction/src/app.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const ses = new AWS.SES();
 const express = require('express');
 const bodyParser = require('body-parser');
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
@@ -20,6 +21,27 @@ app.use(function (req, res, next) {
   next()
 });
 
+const sendEmail = async (subject, message) => {
+  try {
+    await ses
+      .sendEmail({
+        Destination: {
+          ToAddresses: ['mitchhankins@icloud.com'],
+        },
+        Source: 'mitchhankins@icloud.com',
+        Message: {
+          Subject: { Data: `Divy ${subject}` },
+          Body: {
+            Text: { Data: message },
+          },
+        },
+      })
+      .promise();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 app.post('/webhook', async function (req, res) {
   const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({ apiVersion: 'latest' });
   const sig = req.headers['stripe-signature'];
@@ -30,6 +52,7 @@ app.post('/webhook', async function (req, res) {
     switch (event.type) {
       case 'checkout.session.completed':
         const retrievedSession = await stripe.checkout.sessions.retrieve(event.data.object.id, { expand: ["line_items"] });
+
         await cognitoidentityserviceprovider.adminUpdateUserAttributes({
           UserAttributes: [{
             Name: 'custom:subscription',
@@ -46,12 +69,12 @@ app.post('/webhook', async function (req, res) {
           if (err) throw Error(err);
         }).promise();
 
+        sendEmail('checkout.session.completed', JSON.stringify({ event, retrievedSession }, null, 2));
         break;
       case 'invoice.paid':
-        // just add one month to the expires field
-        console.log('invoice paid', req.body);
         break;
       case 'invoice.payment_failed':
+        sendEmail('invoice.payment_failed', JSON.stringify({ event }, null, 2));
         break;
       default:
         return res.status(400).end();
