f78f26b9f1f96fffef24f1ac06ba278732f201b6
Mitch Hankins <mitchhankins@icloud.com>
2021-10-26 12:59:12 -0400

    fixes

--- amplify/backend/function/statisticsFunction/src/index.js
+++ amplify/backend/function/statisticsFunction/src/index.js
@@ -1,10 +1,10 @@
 /* Amplify Params - DO NOT EDIT
-	API_HOLDINGS_GRAPHQLAPIENDPOINTOUTPUT
-	API_HOLDINGS_GRAPHQLAPIIDOUTPUT
-	API_HOLDINGS_STRIPEEVENTTABLE_ARN
-	API_HOLDINGS_STRIPEEVENTTABLE_NAME
-	ENV
-	REGION
+  API_HOLDINGS_GRAPHQLAPIENDPOINTOUTPUT
+  API_HOLDINGS_GRAPHQLAPIIDOUTPUT
+  API_HOLDINGS_STRIPEEVENTTABLE_ARN
+  API_HOLDINGS_STRIPEEVENTTABLE_NAME
+  ENV
+  REGION
 Amplify Params - DO NOT EDIT */
 require('cross-fetch/polyfill');
 const AWS = require('aws-sdk');
@@ -15,45 +15,45 @@ const AWSAppSyncClient = require('aws-appsync').default;
 const fs = require('fs');
 
 const saveEvent = async (id, type, message) => {
-    try {
-      await docClient.put({
-        TableName: process.env.API_HOLDINGS_STRIPEEVENTTABLE_NAME,
-        Item: {
-          id: id,
-          type: type,
-          message: message,
-          updatedAt: new Date().toISOString(),
-          createdAt: new Date().toISOString()
-        }
-      }).promise();
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    await docClient.put({
+      TableName: process.env.API_HOLDINGS_STRIPEEVENTTABLE_NAME,
+      Item: {
+        id: id,
+        type: type,
+        message: message,
+        updatedAt: new Date().toISOString(),
+        createdAt: new Date().toISOString()
+      }
+    }).promise();
+  } catch (error) {
+    console.log(error);
   }
+}
 
 const graphqlClient = new AWSAppSyncClient({
-    url: process.env.API_HOLDINGS_GRAPHQLAPIENDPOINTOUTPUT,
-    region: process.env.AWS_REGION,
-    auth: {
-        type: 'AWS_IAM',
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            sessionToken: process.env.AWS_SESSION_TOKEN
-        }
-    },
-    disableOffline: true
+  url: process.env.API_HOLDINGS_GRAPHQLAPIENDPOINTOUTPUT,
+  region: process.env.AWS_REGION,
+  auth: {
+    type: 'AWS_IAM',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN
+    }
+  },
+  disableOffline: true
 }, {
-    defaultOptions: {
-        query: {
-            fetchPolicy: 'network-only',
-            errorPolicy: 'all',
-        },
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all',
     },
+  },
 });
 
 exports.handler = async (event) => {
-    const query = gql`query HoldingsByOwner(
+  const query = gql`query HoldingsByOwner(
         $owner: String
         $sortDirection: ModelSortDirection
         $filter: ModelHoldingFilterInput
@@ -81,68 +81,70 @@ exports.handler = async (event) => {
         }
       }`;
 
-    const client = await graphqlClient.hydrated();
-    const { data } = await client.query({
-        query,
-        variables: { owner: event.identity.claims[true ? 'sub' : 'cognito:username'] }
-    });
-    const symbols = data.holdingsByOwner.items.map(holding => holding.symbol);
-    const list = [];
+  const client = await graphqlClient.hydrated();
+  const { data } = await client.query({
+    query,
+    variables: { owner: event.identity.claims[true ? 'sub' : 'cognito:username'] }
+  });
+  const symbols = data.holdingsByOwner.items.map(holding => holding.symbol);
+  const list = [];
 
-    if (!symbols.length) {
-        return JSON.stringify(list);
-    }
-
-    const options = {
-        method: 'GET',
-        url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes',
-        params: { region: 'US', symbols: symbols.join(',') },
-        headers: {
-            'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-            'x-rapidapi-key': process.env.YAHOO_KEY
-        }
-    };
-
-    try {
-        // const unparsedData = fs.readFileSync('yahooData', 'utf8')
-        // yahooData = JSON.parse(unparsedData)
-        const { data: yahooData } = await axios.request(options);
-
-        for (const { symbol, regularMarketPrice, dividendRate = 0, dividendYield = 0, ...rest } of yahooData.quoteResponse.result) {
-            // const match = data.holdingsByOwner.items.find(item => replaceAll(item.symbol, '/', '-') === symbol);
-            const match = data.holdingsByOwner.items.find(item => item.symbol === symbol);
-            
-            if (match) {
-                const costBasis = match.price * match.quantity;
-                const marketValue = regularMarketPrice * match.quantity;
-                const gain = marketValue - costBasis;
-
-                list.push({
-                    ...match,
-                    gain,
-                    symbol,
-                    costBasis,
-                    marketValue,
-                    dividendRate,
-                    dividendYield,
-                    buyPrice: match.price,
-                    quantity: match.quantity,
-                    marketPrice: regularMarketPrice,
-                    totalDividends: dividendRate * match.quantity,
-                    id: `${symbol}.${regularMarketPrice}`,
-                    holdingID: match.id
-                });
-            } else {
-                await saveEvent(symbol, 'stats_func_symbol_404', JSON.stringify({ 
-                    symbol,
-                    owner0: event.identity.claims['sub'],
-                    owner1: event.identity.claims['cognito:username']
-                }));
-            }
-        }
-    } catch (error) {
-        console.log(error);
-    }
-
+  if (!symbols.length) {
     return JSON.stringify(list);
+  }
+
+  const options = {
+    method: 'GET',
+    url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes',
+    params: { region: 'US', symbols: symbols.join(',') },
+    headers: {
+      'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+      'x-rapidapi-key': process.env.YAHOO_KEY
+    }
+  };
+
+  try {
+    // const unparsedData = fs.readFileSync('yahooData', 'utf8')
+    // yahooData = JSON.parse(unparsedData)
+    const { data: yahooData } = await axios.request(options);
+
+    for (const { symbol, regularMarketPrice, dividendRate, dividendYield, trailingAnnualDividendRate, trailingAnnualDividendYield, ...rest } of yahooData.quoteResponse.result) {
+      // const match = data.holdingsByOwner.items.find(item => replaceAll(item.symbol, '/', '-') === symbol);
+      const match = data.holdingsByOwner.items.find(item => item.symbol === symbol);
+
+      if (match) {
+        const latestDividendRate = dividendRate || trailingAnnualDividendRate || 0;
+        const latestDividendYield = dividendYield || trailingAnnualDividendYield || 0;
+        const costBasis = match.price * match.quantity;
+        const marketValue = regularMarketPrice * match.quantity;
+        const gain = marketValue - costBasis;
+
+        list.push({
+          ...match,
+          gain,
+          symbol,
+          costBasis,
+          marketValue,
+          dividendRate: latestDividendRate,
+          dividendYield: latestDividendYield,
+          buyPrice: match.price,
+          quantity: match.quantity,
+          marketPrice: regularMarketPrice,
+          totalDividends: latestDividendRate * match.quantity,
+          id: `${symbol}.${regularMarketPrice}`,
+          holdingID: match.id
+        });
+      } else {
+        await saveEvent(symbol, 'stats_func_symbol_404', JSON.stringify({
+          symbol,
+          owner0: event.identity.claims['sub'],
+          owner1: event.identity.claims['cognito:username']
+        }));
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return JSON.stringify(list);
 };
