38bc81493c5eee38da1d591a2eac4aea5feb0527
Mitch Hankins <mitchhankins@icloud.com>
2021-11-08 09:13:43 -0800

    import broker

--- amplify/backend/function/importHoldingsFunction/src/index.js
+++ amplify/backend/function/importHoldingsFunction/src/index.js
@@ -1,4 +1,8 @@
 /* Amplify Params - DO NOT EDIT
+    API_HOLDINGS_GRAPHQLAPIENDPOINTOUTPUT
+    API_HOLDINGS_GRAPHQLAPIIDOUTPUT
+    API_HOLDINGS_HOLDINGTABLE_ARN
+    API_HOLDINGS_HOLDINGTABLE_NAME
     ENV
     REGION
     STORAGE_IMPORTHOLDINGSSTORAGE_BUCKETNAME
@@ -8,108 +12,189 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const xlsx = require('xlsx');
 const axios = require('axios');
+const {
+    chunkArray,
+    holdingsByOwnerQuery,
+    addHoldingMutation,
+    updateHoldingMutation,
+    graphqlClient
+} = require('./helpers');
 
 exports.handler = async (event) => {
-    const params = {
-        Bucket: process.env.STORAGE_IMPORTHOLDINGSSTORAGE_BUCKETNAME,
-        Key: `public/${event.arguments.fileKey}`,
-        // Bucket: 'importholdingsbucket183939-dev',
-        // Key: 'test.xlsx'
-    };
-    const data = await s3.getObject(params).promise();
-    const workbook = xlsx.read(data.Body, { type: 'buffer' });
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonSheet = xlsx.utils.sheet_to_json(worksheet, { blankrows: false, });
-    const existingSymbols = event.arguments.existingSymbols.split(',');
 
-    let symbols = [];
-    const failSymbols = [];
-    const successSymbols = [];
-    const formattedSheet = jsonSheet.map((e, index) => {
-        const formattedSymbol = e.symbol.trim().toUpperCase();
+    const processInput = async ({ symbolKey, quantityKey, priceKey, symbolReplace }) => {
 
-        if (symbols.includes(formattedSymbol)) {
-            failSymbols.push({
-                id: `${formattedSymbol}${index}`,
-                symbol: formattedSymbol,
-                reason: 'Symbol is a duplicate.'
-            });
-        } else if (existingSymbols.includes(formattedSymbol)) {
-            failSymbols.push({
-                id: `${formattedSymbol}${index}`,
-                symbol: formattedSymbol,
-                reason: 'Symbol already exists in portfolio.'
-            });
-        } else {
-            symbols.push(formattedSymbol);
-            return { ...e, id: `${formattedSymbol}${index}`, symbol: formattedSymbol };
-        }
-    }).filter(e => e !== undefined);
+        const client = await graphqlClient.hydrated();
+        const { data } = await client.query({ query: holdingsByOwnerQuery, variables: { owner: event.identity.claims['sub'], limit: 500 } });
+        
+        const params = {
+            Bucket: process.env.STORAGE_IMPORTHOLDINGSSTORAGE_BUCKETNAME,
+            Key: `public/${event.arguments.fileKey}`,
+            // Bucket: 'importholdingsbucket183939-dev',
+            // Key: 'test.xlsx'
+            // Key: 'Portfolio_Positions_Nov-05-2021.csv'
+            // Key: 'tastyworks_positions_mitchhankins_2021-11-04.csv'
+        };
+        const s3Data = await s3.getObject(params).promise();
 
-    const chunkedSymbols = symbols.reduce((resultArray, item, index) => {
-        const chunkIndex = Math.floor(index / 45);
+        const workbook = xlsx.read(s3Data.Body, { type: 'buffer' });
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonSheet = xlsx.utils.sheet_to_json(worksheet, { blankrows: false, });
 
-        if (!resultArray[chunkIndex]) {
-            resultArray[chunkIndex] = [];
-        }
+        const newSymbols = [];
+        const failSymbols = [];
+        const updateSymbols = [];
 
-        resultArray[chunkIndex].push(item);
-        return resultArray;
-    }, []);
+        let yahooToDo = [];
+        const existingSymbols = data.holdingsByOwner.items.map(holding => holding.symbol);
 
-    try {
-        const yahooData = [];
-
-        for (const chunkedItems of chunkedSymbols) {
-            const { data: { quoteResponse } } = await axios.request({
-                method: 'GET',
-                url: `https://${process.env.YAHOO_HOST}/market/v2/get-quotes`,
-                params: { region: 'US', symbols: chunkedItems.join(',') },
-                headers: {
-                    'x-rapidapi-host': process.env.YAHOO_HOST,
-                    'x-rapidapi-key': process.env.YAHOO_KEY
-                }
-            });
-            yahooData.push(...quoteResponse.result);
-        }
-
-        yahooData.forEach((yahooItem, index) => {
-            symbols = symbols.filter(e => e !== yahooItem.symbol);
-
-            const sheetItem = formattedSheet.find(e => e.symbol === yahooItem.symbol);
-            const parsedNumberOfShares = Number(sheetItem.numberOfShares);
-            const parsedPricePerShare = Number(sheetItem.pricePerShare);
-
-            if (isNaN(parsedNumberOfShares)) {
+        const seenSymbols = new Set();
+        const uniques = jsonSheet.filter((e, i) => {
+            if (seenSymbols.has(e[symbolKey])) {
                 failSymbols.push({
-                    symbol: sheetItem.symbol,
-                    id: `${sheetItem.symbol}${index}`,
-                    reason: `Number of shares is not valid, got "${sheetItem.numberOfShares}".`
+                    id: `${e[symbolKey]}-${i}`,
+                    symbol: e[symbolKey],
+                    reason: 'Symbol is a duplicate in the import file.'
                 });
-            } else if (isNaN(parsedPricePerShare)) {
-                failSymbols.push({
-                    symbol: sheetItem.symbol,
-                    id: `${sheetItem.symbol}${index}`,
-                    reason: `Price per share is not valid, got "${sheetItem.pricePerShare}".`
-                });
-            } else {
-                successSymbols.push({
-                    ...sheetItem,
-                    id: `${sheetItem.symbol}${index}`,
-                    numberOfShares: parsedNumberOfShares,
-                    pricePerShare: parsedPricePerShare,
-                    comments: `${sheetItem.comments} - Automatically imported by Divy`
-                });
+                return false;
+            } else if (e[symbolKey] && e[quantityKey]) {
+                seenSymbols.add(e[symbolKey]);
+                return true;
             }
         });
 
-        symbols.forEach((symbol, index) => {
-            failSymbols.push({ symbol, id: `${symbol}${index}`, reason: 'Symbol could not be found.' });
-        });
-    } catch (error) {
-        console.log(error.message);
-        throw new Error('Corrupt spreadsheet.')
+        for (const e of uniques) {
+            const symbol = e[symbolKey].replace(symbolReplace, '-');
+            const quantity = Number(e[quantityKey]);
+            const tradePrice = Math.abs(e[priceKey]);
+
+            if (existingSymbols.includes(symbol)) {
+                const match = data.holdingsByOwner.items.find(item => item.symbol === symbol);
+
+                if (quantity === Number(match.quantity) && tradePrice === Number(match.price)) {
+                    failSymbols.push({
+                        id: match.id,
+                        symbol: match.symbol,
+                        reason: 'Symbol already exists in portfolio and has not changed in price or quantity.'
+                    });
+                } else {
+                    if (quantity === Number(match.quantity)) {
+                        updateSymbols.push({
+                            symbol,
+                            id: match.id,
+                            numberOfShares: quantity,
+                            pricePerShare: tradePrice,
+                            reason: `Price changed from $${match.price} to $${tradePrice}.`
+                        });
+                    } else if (tradePrice === Number(match.price)) {
+                        updateSymbols.push({
+                            symbol,
+                            id: match.id,
+                            numberOfShares: quantity,
+                            pricePerShare: tradePrice,
+                            reason: `Quantity changed from ${match.quantity} to ${quantity}.`
+                        });
+                    } else {
+                        updateSymbols.push({
+                            symbol,
+                            id: match.id,
+                            numberOfShares: quantity,
+                            pricePerShare: tradePrice,
+                            reason: `Price changed from $${match.price} to $${tradePrice} and quantity changed from ${match.quantity} to ${quantity}.`
+                        });
+                    }
+                    await client.mutate({
+                        mutation: updateHoldingMutation,
+                        variables: {
+                            input: {
+                                id: match.id,
+                                price: tradePrice,
+                                symbol: match.symbol,
+                                comments: match.comments,
+                                quantity: quantity,
+                            }
+                        }
+                    });
+                }
+            } else {
+                yahooToDo.push({ symbol, quantity, tradePrice });
+            }
+        }
+
+        try {
+            const chunkedYahooToDo = chunkArray(yahooToDo);
+
+            for (const chunkedItems of chunkedYahooToDo) {
+                const mappedSymbols = chunkedItems.map(e => e.symbol);
+                const { data: { quoteResponse } } = await axios.request({
+                    method: 'GET',
+                    url: `https://${process.env.YAHOO_HOST}/market/v2/get-quotes`,
+                    params: { region: 'US', symbols: mappedSymbols.join(',') },
+                    headers: {
+                        'x-rapidapi-host': process.env.YAHOO_HOST,
+                        'x-rapidapi-key': process.env.YAHOO_KEY
+                    }
+                });
+
+                for (const yahooItem of quoteResponse.result) {
+                    yahooToDo = yahooToDo.filter(e => e.symbol !== yahooItem.symbol);
+                    const chunkedItem = chunkedItems.find(e => e.symbol === yahooItem.symbol);
+
+                    await client.mutate({
+                        mutation: addHoldingMutation,
+                        variables: {
+                            input: {
+                                comments: '',
+                                symbol: chunkedItem.symbol,
+                                price: chunkedItem.tradePrice,
+                                quantity: chunkedItem.quantity,
+                                owner: event.identity.claims['sub'],
+                            }
+                        }
+                    });
+                    newSymbols.push({
+                        id: `${yahooItem.symbol}-${yahooItem.regularMarketPrice}`,
+                        symbol: chunkedItem.symbol,
+                        numberOfShares: chunkedItem.quantity,
+                        pricePerShare: chunkedItem.tradePrice,
+                        comments: 'Automatically imported by Divy'
+                    });
+                }
+            }
+
+            yahooToDo.forEach((e, index) => {
+                failSymbols.push({ symbol: e.symbol, id: `${e.symbol}${index}`, reason: 'Symbol could not be found.' });
+            });
+        } catch (error) {
+            console.log(error.message);
+            return { newSymbols: [], failSymbols: [], updateSymbols: [] };
+        }
+        return { newSymbols, failSymbols, updateSymbols };
     }
 
-    return { successSymbols, failSymbols };
+    switch (event.arguments.type) {
+        case 'tastyworks':
+            return await processInput({
+                symbolKey: 'Symbol',
+                quantityKey: 'Quantity',
+                priceKey: 'Trade Price',
+                symbolReplace: '/'
+            });
+        case 'fidelity':
+            return await processInput({
+                symbolKey: 'Symbol',
+                quantityKey: 'Quantity',
+                priceKey: 'Cost Basis Per Share',
+                symbolReplace: '/'
+            });
+        case 'spreadsheet':
+            return await processInput({
+                symbolKey: 'symbol',
+                quantityKey: 'numberOfShares',
+                priceKey: 'pricePerShare',
+                symbolReplace: undefined
+            });
+        default:
+            return { newSymbols: [], failSymbols: [], updateSymbols: [] };
+    }
 };
--- src/views/holdings/HoldingsImportView/Review.js
+++ src/views/holdings/HoldingsImportView/Review.js
@@ -10,13 +10,8 @@ import {
     ListItem,
     ListItemIcon,
     ListItemText,
-    LinearProgress
 } from '@material-ui/core';
-import { Warning, Check } from '@material-ui/icons';
-import { useHistory } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
-import { createHolding } from 'src/graphql/mutations';
-import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { Warning, Check, Cached } from '@material-ui/icons';
 import useData from 'src/hooks/useData';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,68 +22,19 @@ const useStyles = makeStyles((theme) => ({
 
 export default (props) => {
     const classes = useStyles();
-    const history = useHistory();
-    const { processRefetch } = useData();
-    const { enqueueSnackbar } = useSnackbar();
-    const [error, setError] = React.useState(null);
-    const [loading, setLoading] = React.useState(false);
-    // function timeout(ms) {
-    //     return new Promise(resolve => setTimeout(resolve, ms));
-    // }
-    const onSubmitFinishReview = async () => {
-        try {
-            setLoading(true);
-            const { attributes } = await Auth.currentAuthenticatedUser();
-
-            for (const item of props.data.successSymbols) {
-                await API.graphql(graphqlOperation(createHolding, {
-                    input: {
-                        price: item.pricePerShare,
-                        comments: item.comments,
-                        quantity: item.numberOfShares,
-                        owner: attributes.sub,
-                        symbol: item.symbol,
-                    }
-                }));
-            }
-
-            processRefetch();
-            enqueueSnackbar('Import Complete', { variant: 'success' });
-            history.push('/app/holdings');
-        } catch (error) {
-            setLoading(false);
-            setError(JSON.stringify(error));
-        }
-    }
+    const { loading } = useData();
 
     return (
         <Card className={classes.root}>
             <Box p={3}>
                 <Grid container spacing={3}>
-                    <Grid item lg={2}>
+                    <Grid item lg={12}>
                         <Button disabled={loading} variant='outlined' onClick={props.resetHoldingsResponse}>Go Back</Button>
                     </Grid>
-                    <Grid item lg={7}>
-                        {loading && <LinearProgress />}
-                        <Typography variant="body1" color='error'>
-                            {loading ?
-                                'Loading, please do not navigate away from this page to prevent errors.' :
-                                'NOTE: Your holdings will not be added to your portfolio untill you\'ve selected the "Finish Review" button.'
-                            }
-                        </Typography>
-                    </Grid>
-                    {error && (
-                        <Box p={1}>
-                            <Typography variant="h4" color='error'>{error}</Typography>
-                        </Box>
-                    )}
-                    <Grid item lg={3}>
-                        <Button style={{ float: 'right' }} disabled={!props.data.successSymbols.length || loading} onClick={onSubmitFinishReview} variant='contained' color='secondary'>Finish Review</Button>
-                    </Grid>
-                    <Grid item lg={6}>
-                        <Typography variant="h3">Errors</Typography>
+                    <Grid item lg={4}>
+                        <Typography variant="h4">Ignored</Typography>
                         <Typography variant="body1">
-                            These holdings could not be processed and will not be added once you submit.
+                            These holdings have been ignored.
                         </Typography>
                         <List dense={true}>
                             {((props && props.data && props.data.failSymbols) || []).map(item => (
@@ -97,27 +43,46 @@ export default (props) => {
                                         <Warning />
                                     </ListItemIcon>
                                     <ListItemText
-                                        primary={item.reason}
-                                        secondary={item.symbol}
+                                        primary={item.symbol}
+                                        secondary={item.reason}
                                     />
                                 </ListItem>
                             ))}
                         </List>
                     </Grid>
-                    <Grid item lg={6}>
-                        <Typography variant="h3">Success</Typography>
+                    <Grid item lg={4}>
+                        <Typography variant="h4">Updated</Typography>
                         <Typography variant="body1">
-                            These holdings were processed successfully and will be added once you submit.
+                            These holdings have been updated.
                         </Typography>
                         <List dense={true}>
-                            {((props && props.data && props.data.successSymbols) || []).map(item => (
+                            {((props && props.data && props.data.updateSymbols) || []).map(item => (
+                                <ListItem key={item.id}>
+                                    <ListItemIcon>
+                                        <Cached />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={item.symbol}
+                                        secondary={item.reason}
+                                    />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Grid>
+                    <Grid item lg={4}>
+                        <Typography variant="h4">Added</Typography>
+                        <Typography variant="body1">
+                            These holdings have been added.
+                        </Typography>
+                        <List dense={true}>
+                            {((props && props.data && props.data.newSymbols) || []).map(item => (
                                 <ListItem key={item.id}>
                                     <ListItemIcon>
                                         <Check />
                                     </ListItemIcon>
                                     <ListItemText
-                                        primary={item.reason}
-                                        secondary={item.symbol}
+                                        primary={item.symbol}
+                                        secondary={item.reason}
                                     />
                                 </ListItem>
                             ))}
