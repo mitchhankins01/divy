f1c6dbe8acb87c1233e053ac90eb8c1054adba75
Mitch Hankins <mitchhankins@icloud.com>
2021-11-08 09:13:43 -0800

    import broker

--- src/views/holdings/HoldingsImportView/index.js
+++ src/views/holdings/HoldingsImportView/index.js
@@ -7,6 +7,12 @@ import {
     Button,
     Typography,
     LinearProgress,
+    Tabs,
+    Tab,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem
 } from '@material-ui/core';
 import { CloudUpload } from '@material-ui/icons';
 import { API, graphqlOperation, Storage } from 'aws-amplify';
@@ -15,7 +21,6 @@ import useData from 'src/hooks/useData';
 import Page from 'src/components/Page';
 import { importHoldings } from 'src/graphql/queries';
 import Review from './Review';
-// import { deleteHolding } from 'src/graphql/mutations';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,26 +30,25 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(3),
         backgroundColor: theme.palette.background.dark,
     },
+    brokerFormControl: {
+        minWidth: 200,
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    }
 }));
 
 export default () => {
     const classes = useStyles();
-    const { listStatistics } = useData();
     const [error, setError] = React.useState(null);
+    const [tabIndex, setTabIndex] = React.useState(0);
     const [loading, setLoading] = React.useState(false);
+    const { listStatistics, processRefetch } = useData();
+    const [selectedBroker, setSelectedBroker] = React.useState('');
     const [importHoldingsResponse, setImportHoldingsResponse] = React.useState(null);
 
     const onUploadFile = async (event) => {
-        // const { data } = await API.graphql(graphqlOperation(listHoldings));
-
-        // for (const item of data?.listHoldings?.items) {
-        //     await API.graphql(graphqlOperation(deleteHolding, { input: { id: item.id } }));
-        //     console.log(`Deleted ${item.symbol}`);
-        // }
-        // processRefetch();
-        // return;
-
         try {
+            setError(null);
             setLoading(true);
             const selectedFile = event.target.files[0];
             const existingSymbols = listStatistics.data.map(item => item.symbol).join(',');
@@ -52,14 +56,23 @@ export default () => {
             formData.append('file', selectedFile);
             // const s3Result = await Storage.put(selectedFile.name, selectedFile, { contentType: selectedFile.type });
             const s3Result = await Storage.put(`${new Date().getTime()}.xlsx`, selectedFile, { contentType: selectedFile.type });
-            const { data } = await API.graphql(graphqlOperation(importHoldings, { existingSymbols, fileKey: s3Result.key }));
+            const { data } = await API.graphql(graphqlOperation(importHoldings, { type: selectedBroker, existingSymbols, fileKey: s3Result.key }));
             setImportHoldingsResponse(data.importHoldings);
             await Storage.remove(s3Result.key);
+            if (data.importHoldings.newSymbols.length || data.importHoldings.updateSymbols.length) {
+                processRefetch();
+            }
         } catch (error) {
             console.log(error);
             setLoading(false);
-            setError('Your spreadsheet is malformed, please try again or use the chat to get help.');
+            setError('Your upload is malformed, please try again or use the chat to get help.');
         }
+    };
+
+    const handleTabChange = (_, newValue) => {
+        setError(null);
+        setTabIndex(newValue);
+        setSelectedBroker(newValue === 0 ? '' : 'spreadsheet');
     };
 
     const resetHoldingsResponse = () => {
@@ -71,6 +84,113 @@ export default () => {
         return <Review data={importHoldingsResponse} resetHoldingsResponse={resetHoldingsResponse} />
     }
 
+    const renderSpreadSheetImport = () => (
+        <>
+            <Box p={1}>
+                <Typography variant="body1">
+                    Step 1:
+                    <Button disabled={loading} style={{ marginLeft: 5 }} variant="outlined" size='small' component="a" href='/import.xlsx' download>
+                        Download the spreadsheet template
+                    </Button>
+                </Typography>
+            </Box>
+            <Box p={1}>
+                <Typography variant="body1">
+                    Step 2: Under the header "symbol" list your holdings.
+                </Typography>
+                <Typography color='textPrimary' variant="body1" component="a" href="https://finance.yahoo.com/" rel="noopener noreferrer" target="_blank">
+                    NOTE: You must use the same structure as Yahoo Finance, click here to check your symbol.
+                </Typography>
+            </Box>
+            <Box p={1}>
+                <Typography variant="body1">
+                    Step 3: Under the header "numberOfShares" enter the number of shares you own per holding.
+                </Typography>
+            </Box>
+            <Box p={1}>
+                <Typography variant="body1">
+                    Step 4: Under the header "pricePerShare" enter your cost basis per holding.
+                </Typography>
+            </Box>
+            <Box p={1}>
+                <Typography variant="body1">
+                    Step 5: Upload your spreadsheet.
+                </Typography>
+            </Box>
+            <Box p={1}>
+                <Typography variant="body1">
+                    Step 6: Review the import.
+                </Typography>
+            </Box>
+            <Box p={1}>
+                <input
+                    accept=".xlsx"
+                    className={classes.input}
+                    id="contained-button-file"
+                    hidden
+                    type="file"
+                    onChange={onUploadFile}
+                />
+                <label htmlFor="contained-button-file">
+                    <Button variant="outlined" color="primary" component="span" startIcon={<CloudUpload />} disabled={Boolean(loading)}>
+                        {loading ? 'Processing' : 'Upload My Spreadsheet'}
+                    </Button>
+                </label>
+            </Box>
+        </>
+    );
+
+    const renderBrokerImport = () => {
+        const handleSelectBrokerChange = (event) => {
+            setSelectedBroker(event.target.value);
+        };
+
+        return (
+            <>
+                <Box p={1}>
+                    <Typography variant="body1">
+                        Step 1: Select your broker. <br /> If it is not listed {'\n'}
+                        <Typography color='textPrimary' variant="body1" component="a" href='mailto:support@divytracker.com?subject=Add Broker Import' rel="noopener noreferrer">
+                            click here
+                        </Typography>
+                        {' '}to send an email and attach your broker's CSV file, it will only take a few days to add support for your broker.
+                    </Typography>
+                    <FormControl className={classes.brokerFormControl}>
+                        <InputLabel>Broker</InputLabel>
+                        <Select disabled={loading} value={selectedBroker} onChange={handleSelectBrokerChange}>
+                            {[
+                                { value: 'fidelity', label: 'Fidelity' },
+                                { value: 'tastyworks', label: 'TastyWorks' },
+                            ].map(e => (
+                                <MenuItem value={e.value} key={e.value}>{e.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box p={1}>
+                    <Typography variant="body1">
+                        Step 2: Upload your broker's CSV file and click the "Upload My Broker CSV" button.
+                    </Typography>
+                </Box>
+                <Box p={1}>
+                    <input
+                        accept=".csv"
+                        className={classes.input}
+                        id="contained-button-file"
+                        hidden
+                        type="file"
+                        onChange={onUploadFile}
+                    />
+                    <label htmlFor="contained-button-file">
+                        <Button variant="outlined" color="primary" component="span" startIcon={<CloudUpload />} disabled={Boolean(loading) || !selectedBroker}>
+                            {loading ? 'Processing' : 'Upload My Broker CSV'}
+                        </Button>
+                    </label>
+                </Box>
+            </>
+        );
+    };
+
     return (
         <Page className={classes.root} title='Import'>
             <Header />
@@ -78,6 +198,10 @@ export default () => {
                 <Box mt={2}>
                     <Card>
                         {loading && <LinearProgress />}
+                        <Tabs value={tabIndex} variant='fullWidth' onChange={handleTabChange}>
+                            <Tab disabled={loading} value={0} label='Broker Import' />
+                            <Tab disabled={loading} value={1} label='Spreadsheet Import' />
+                        </Tabs>
                         <Box p={2}>
                             {error && (
                                 <Box p={1}>
@@ -87,60 +211,7 @@ export default () => {
                             <Box p={1}>
                                 <Typography variant="h3">Instructions</Typography>
                             </Box>
-                            <Box p={1}>
-                                <Typography variant="body1">
-                                    Step 1:
-                                    <Button disabled={loading} style={{ marginLeft: 5 }} variant="outlined" size='small' component="a" href='/import.xlsx' download>
-                                        Download the spreadsheet template
-                                    </Button>
-                                </Typography>
-                            </Box>
-                            <Box p={1}>
-                                <Typography variant="body1">
-                                    Step 2: Under the header "symbol" list your holdings.
-                                </Typography>
-                                <Typography color='textPrimary' variant="body1" component="a" href="https://finance.yahoo.com/" rel="noopener noreferrer" target="_blank">
-                                    NOTE: You must use the same structure as Yahoo Finance, click here to check your symbol.
-                                </Typography>
-                            </Box>
-                            <Box p={1}>
-                                <Typography variant="body1">
-                                    Step 3: Under the header "numberOfShares" enter the number of shares you own per holding.
-                                </Typography>
-                            </Box>
-                            <Box p={1}>
-                                <Typography variant="body1">
-                                    Step 4: Under the header "pricePerShare" enter your cost basis per holding.
-                                </Typography>
-                            </Box>
-                            <Box p={1}>
-                                <Typography variant="body1">
-                                    Step 5: Upload your spreadsheet.
-                                </Typography>
-                                <Typography variant="body1">
-                                    NOTE: In the next screen you must review the result of the import before the holdings are added to your portfolio.
-                                </Typography>
-                            </Box>
-                            <Box p={1}>
-                                <Typography variant="body1">
-                                    Step 6: Review the import and make sure to submit it to add the holdings to your portfolio.
-                                </Typography>
-                            </Box>
-                            <Box p={1}>
-                                <input
-                                    accept=".xlsx"
-                                    className={classes.input}
-                                    id="contained-button-file"
-                                    hidden
-                                    type="file"
-                                    onChange={onUploadFile}
-                                />
-                                <label htmlFor="contained-button-file">
-                                    <Button variant="outlined" color="primary" component="span" startIcon={<CloudUpload />} disabled={Boolean(loading)}>
-                                        {loading ? 'Processing' : 'Upload My Spreadsheet'}
-                                    </Button>
-                                </label>
-                            </Box>
+                            {tabIndex === 0 ? renderBrokerImport() : renderSpreadSheetImport()}
                         </Box>
                     </Card>
                 </Box>
